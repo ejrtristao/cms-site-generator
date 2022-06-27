4564818830a46e03b86051911a012c6404435760
ejrtristao <ejrtristao@gmail.com>
2022-06-27 17:41:37 -0300

    refactor remove unuses files

--- admin/layouts/footer.php
+++ admin/layouts/footer.php
@@ -6,15 +6,7 @@
         <span class="text-muted">CMS - Generator</span>
     </div>
 </footer>
-<!-- Include the Quill library -->
-<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
 
-<!-- Initialize Quill editor -->
-<script>
-  var quill = new Quill('#editor', {
-    theme: 'snow'
-  });
-</script>
 <script src="https://getbootstrap.com/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
