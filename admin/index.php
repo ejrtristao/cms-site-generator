7ca8a78695e4a281904239ef295e300903de49ee
ejrtristao <ejrtristao@gmail.com>
2022-06-27 17:41:56 -0300

    feat include title site in home page

--- admin/index.php
+++ admin/index.php
@@ -2,9 +2,11 @@
 include './layouts/header.php';
 $db = new SQLite3('./db/sites.db');
 $list = $db->query('SELECT * FROM sites');
+$row = $list->fetchArray();
 ?>
 <div>
     <h3>Configurar Site</h3>
+    <h4><?php echo $row['title'] ?></h4>
 </div>
 <?php
 include './layouts/footer.php';
